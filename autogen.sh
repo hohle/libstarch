47d8de7d66d608976f4403bbb2ca056b2ad041b7
Jonathan Hohle <jonhohle@gmail.com>
2019-08-16 17:18:55 -0700

    libtoolizegit add autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,12 @@
 mkdir -p build-aux # this should be something else
-libtoolize
+
+case `uname` in
+  Darwin*)
+      glibtoolize --copy ;;
+  *)
+      libtoolize --copy ;;
+esac
+
 aclocal
 automake --add-missing --force-missing --copy --foreign
 autoreconf
